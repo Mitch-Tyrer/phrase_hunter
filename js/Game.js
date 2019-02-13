1ae40383a194bcafd36b4e927d3b4926e783755f
Mitch-Tyrer <mltcinematography@gmail.com>
2019-02-13 14:17:19 -0700

    initialized base documents. Added phrase generation and random function to game state

---
+++ js/Game.js
@@ -0,0 +1,36 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    /**
+     * Creates an array of phrases for the game
+     * @return {array} An array of phrases to be used in the game
+     */
+    createPhrases() {
+        let phrases = [];
+        phrases.push(new Phrase('May the Force be with you!'));
+        phrases.push(new Phrase('Winter is Coming'));
+        phrases.push(new Phrase('Veritas Aequitas'));
+        phrases.push(new Phrase('love'));
+        phrases.push(new Phrase('May the Force be with you!'));
+
+        return phrases;
+    }
+
+    /**
+     * selects a random phrase from the array
+     * @return {Object} phrase object to be used
+     */
+    getRandomPhrase() {
+        const ranNum = Math.floor(Math.random() * 5);
+        return this.phrases[ranNum];
+    }
+
+}
---
+++ js/app.js
@@ -0,0 +1,20 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * app.js */
+
+ const phrase = new Phrase('Life is like a box of Chocolates');
+ const game = new Game();
+
+game.phrases.forEach((phrase, index) => {
+    console.log(`Phrase ${index} phrase: ${phrase.phrase}`);
+});
+
+const logPhrase = (phrase) => {
+    console.log(`Phrase - phrase: `, phrase.phrase);
+    };
+
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
